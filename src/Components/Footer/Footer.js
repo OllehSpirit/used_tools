1cc91046002247be97409f35e924ccd8f7fd9478
AnasAttoum <144582953+OllehSpirit@users.noreply.github.com>
2023-09-27 16:01:12 +0300

    footer responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,20 @@
-import SocialMediaIcons from '../SocialMediaIcons/SocialMediaIcons';
-import classes from './Footer.module.css'
+import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
+import classes from "./Footer.module.css";
 
 function StartingPage() {
-    return (
-      <div className={classes.footer}>
-        <img src='Images/Logo.png' alt='Logo' className={classes.img}/>
-        <p className={classes.contact}>For more information please contact us :</p>
-        <SocialMediaIcons/>
-        
-        <p className={classes.copy}>Copyright © Used Store 2023</p>
-      </div>
-    );
-  }
-  
-  export default StartingPage;
+  return (
+    <div className={classes.footer}>
+      <p className={classes.contact}>
+        For more information please contact us :
+      </p>
+      <SocialMediaIcons />
+
+      <img src="Images/Logo.png" alt="Logo" className={classes.img} />
+
+      <p className={classes.copy}>Copyright © Used Store 2023</p>
+      <br/>
+    </div>
+  );
+}
+
+export default StartingPage;
